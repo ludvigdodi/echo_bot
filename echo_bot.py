5ffad66c331eb58f06e92f0b64fe153f31ecee30
Ludvig Dodi <ludvigdodi.job@gmail.com>
2022-09-18 20:18:09 +0300

    echo_bot added

---
+++ echo_bot.py
@@ -0,0 +1,28 @@
+import requests
+
+TOKEN = "5363676717:AAGKphYhwjfWavyybT03t0g0UBcELX6mVuc"
+
+ROOT_URL = f"https://api.telegram.org/bot{TOKEN}"
+
+
+def get_update(url, update_id):
+    responce = requests.get(f"{url}/getUpdates?offset={update_id + 1}")
+    return responce.json()
+
+
+def send_message(chat_id, message_text, url):
+    data = {"chat_id": chat_id, "text": message_text}
+    responce = requests.post(f"{url}/sendMessage", data=data)
+
+
+answered_update_id = 0
+
+while True:
+    updates = get_update(ROOT_URL, answered_update_id)
+    if updates.get("result"):
+        update_id = updates.get("result")[0]["update_id"]
+        if update_id != answered_update_id:
+            chat_id = updates.get("result")[0]["message"]["chat"]["id"]
+            message_text = updates.get("result")[0]["message"]["text"]
+            send_message(chat_id, message_text, ROOT_URL)
+            answered_update_id = update_id
