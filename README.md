09ce168dab269c8e2e45fd0e0b9e64ec97153908
unknown <ludvigdodi@gmail.com>
2023-01-07 21:32:16 +0200

    modified echo_bot.py README.md

--- README.md
+++ README.md
@@ -1,40 +1,30 @@
 # @LudvigDodiBot
 
-This Telegram bot helps you learn English.
+This is simple Telegram ECHO bot.
 
 The principle of interaction with it is very simple: for the word you enter, it returns several sentences with this word according to your level of language knowledge.
 
 
 
 ## Run localy
-- make a clone of the project or download it as a zip archive
-- unzip it to your computer
+- clone project to your computer
+
 - install dependencies 
 
-```sh
-pip install -r requirements.txt
-```
-
-```sh
-Create bot_token.py file with 'token' variabel or paste your token to 'token' in app.py file (7 row)
-```
+    ```sh
+    pip install -r requirements.txt
+    ``` 
+    
+- import token to echho_bot.py file
+    ```sh
+    Create .env file with 'token' variabel 
+    ```
 
 - go to the terminal (Linux or MacOS) or powershell if you are on Windows and enter the command
 
 ```sh
-Windows:
 python app.py
 ```
-
-```sh
-MacOS:
-python app.py
-```
-```sh
-Linux:
-python app.py
-```
-
 
 ## License
 
--- echo_bot.py
+++ echo_bot.py
@@ -4,9 +4,10 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+
+
 TOKEN = os.environ.get("TOKEN")
 ROOT_URL = f"https://api.telegram.org/bot{TOKEN}"
-
 
 
 def get_update(url, update_id):
@@ -18,15 +19,18 @@ def send_message(chat_id, message_text, url):
     data = {"chat_id": chat_id, "text": message_text}
     responce = requests.post(f"{url}/sendMessage", data=data)
 
+def pooling():
+    answered_update_id = 0
 
-answered_update_id = 0
+    while True:
+        updates = get_update(ROOT_URL, answered_update_id)
+        if updates.get("result"):
+            update_id = updates.get("result")[0]["update_id"]
+            if update_id != answered_update_id:
+                chat_id = updates.get("result")[0]["message"]["chat"]["id"]
+                message_text = updates.get("result")[0]["message"]["text"]
+                send_message(chat_id, message_text, ROOT_URL)
+                answered_update_id = update_id
 
-while True:
-    updates = get_update(ROOT_URL, answered_update_id)
-    if updates.get("result"):
-        update_id = updates.get("result")[0]["update_id"]
-        if update_id != answered_update_id:
-            chat_id = updates.get("result")[0]["message"]["chat"]["id"]
-            message_text = updates.get("result")[0]["message"]["text"]
-            send_message(chat_id, message_text, ROOT_URL)
-            answered_update_id = update_id
+if __name__ == '__main__':
+    pooling()
