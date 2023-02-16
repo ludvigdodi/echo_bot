7305a61cff24027a575a36ab463b6118f0fe5ffc
unknown <ludvigdodi@gmail.com>
2023-02-16 11:52:24 +0200

    midified echobot

--- echo_bot.py
+++ echo_bot.py
@@ -14,9 +14,12 @@ def get_update(url, update_id):
     responce = requests.get(f"{url}/getUpdates?offset={update_id + 1}")
     return responce.json()
 
-
 def send_message(chat_id, message_text, url):
     data = {"chat_id": chat_id, "text": message_text}
+    responce = requests.post(f"{url}/sendMessage", data=data)
+
+def start(chat_id, message_text, url):
+    data = {"chat_id": chat_id, "text": '/start'}
     responce = requests.post(f"{url}/sendMessage", data=data)
 
 def pooling():
@@ -29,7 +32,10 @@ def pooling():
             if update_id != answered_update_id:
                 chat_id = updates.get("result")[0]["message"]["chat"]["id"]
                 message_text = updates.get("result")[0]["message"]["text"]
-                send_message(chat_id, message_text, ROOT_URL)
+                if message_text == '/start':
+                    start(chat_id, message_text, ROOT_URL)
+                else:
+                    send_message(chat_id, message_text, ROOT_URL)
                 answered_update_id = update_id
 
 if __name__ == '__main__':
